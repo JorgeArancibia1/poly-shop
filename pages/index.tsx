a7c6fdcceb1cbaf67becc3fff3409bbfb2ef58c0
Jorge <jarancibia@waypoint.com>
2022-04-30 17:20:55 -0400

    Material ui ok, base layout ok, eslint running

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
-import type { NextPage } from 'next'
+import { Typography } from '@mui/material';
+import type { NextPage } from 'next';
+import { ShopLayout } from '../components/layouts/ShopLayout';
 
 const Home: NextPage = () => {
-  return (
-    <h1>Poly Shop</h1>
-  )
-}
+	return (
+		<ShopLayout
+			title={'Poly-Shop'}
+			pageDescription={'Encuentra los mejores productos de ropa americana.'}
+		>
+			<Typography variant='h1' component='h1'>
+Tienda
+			</Typography>
+			<Typography variant='h2' sx={{ mb: 1 }}>
+Todos los productos
+			</Typography>
+		</ShopLayout>
+	);
+};
 
-export default Home
+export default Home;
