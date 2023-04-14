8902f8e02644c46347afd4ba824994e8e314b254
Jorge <jorge.arancibia.jeal@gmail.com>
2023-04-14 13:50:14 -0400

    images added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { Typography } from '@mui/material';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
 import type { NextPage } from 'next';
 import { ShopLayout } from '../components/layouts/ShopLayout';
+import { initialData } from '../database/products';
 
 const Home: NextPage = () => {
 	return (
@@ -11,9 +12,29 @@ const Home: NextPage = () => {
 			<Typography variant='h1' component='h1'>
 				Tienda
 			</Typography>
-			<Typography variant='h2' sx={{ mb: 1 }}>
+			<Typography variant='h2' sx={{ mb:1 }}>
 				Todos los productos
 			</Typography>
+			Algo
+
+			<Grid container spacing={4}>
+				{
+					initialData.products.map( product => (
+						<Grid item xs={6} sm={4} key={ product.slug }>
+							<Card>
+								<CardActionArea>
+									<CardMedia
+										component='img'
+										image={`products/${product.images[0]}`}
+										alt={product.title}
+									/>
+								</CardActionArea>
+							</Card>
+						</Grid>
+					) )
+				}
+			</Grid>
+
 		</ShopLayout>
 	);
 };
